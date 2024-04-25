76a98dd666e1c1c49509d56a908875991b0ec78f
DanielDlc <dlc.engcomputacao@gmail.com>
2024-04-25 21:07:23 +0100

    banco de dados

--- SQLAlchemy/gusqla/03sqla_sync/conf/db_session.py
+++ SQLAlchemy/gusqla/03sqla_sync/conf/db_session.py
@@ -27,36 +27,35 @@ def create_engine(sqlite: bool = False) -> Engine:
         folder.mkdir(parents=True, exist_ok=True)
 
         conn_str = f'sqlite:///{arquivo_db}'
-        __engine = sa.create_engine(url=conn_str, echo=False,
-                                     connect_args={"check_same_thread": False})
+        __engine = sa.create_engine(url=conn_str, echo=False, connect_args={"check_same_thread": False})
     else:
-        conn_str = "postegresql://danieldlc:miguel@localhost:5432/picoles"
+        conn_str = "postgresql://danieldlc:miguel@localhost:5432/picoles"
         __engine = sa.create_engine(url=conn_str, echo=False)
 
     return __engine
 
 def create_session() -> Session:
     """
-    create conection database
+    Função para criar sessão de conexao ao banco de dados.
     """
-
     global __engine
 
-    if __engine:
-        create_engine()
-
+    if not __engine:
+        create_engine() # create_engine(sqlite=True)
+    
     __session = sessionmaker(__engine, expire_on_commit=False, class_=Session)
 
     session: Session = __session()
 
     return session
 
+
 def create_tables() -> None:
     global __engine
 
     if not __engine:
-        create_engine()
-
-    import models. __all__models
+        create_engine(sqlite=True)
+    
+    import models.__all__models
     ModelBase.metadata.drop_all(__engine)
     ModelBase.metadata.create_all(__engine)
